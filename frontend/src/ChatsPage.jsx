0b3ca4d59b1e14b6da0dc08d59269ec831681ab3
Aung Kaung Myint <aungkggmyint@gmail.com>
2024-09-20 13:51:23 +0700

    connected backend-frontend

--- frontend/src/ChatsPage.jsx
+++ frontend/src/ChatsPage.jsx
@@ -1,5 +1,19 @@
-const ChatsPage = () => {
-    return <>Chats...</>
+import { MultiChatSocket, MultiChatWindow, useMultiChatLogic } from 'react-chat-engine-advanced'
+
+const ChatsPage = (props) => {
+
+    const chatProps = useMultiChatLogic( 
+        '3c4c86ce-8ae6-4911-8865-e374bd9f806a', 
+        props.user.username, 
+        props.user.secret
+    );
+
+    return (
+    <div style={{ height: '100vh' }}>
+        <MultiChatSocket {...chatProps} />
+        <MultiChatWindow {...chatProps} style={{height: '100%'}} />
+    </div>
+    )
 }
 
 export default ChatsPage
