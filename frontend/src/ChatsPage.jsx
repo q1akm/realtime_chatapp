ede29f1010c6dbbe341385600aaeb54ffa3f7f66
Aung Kaung Myint <aungkggmyint@gmail.com>
2024-09-20 14:21:33 +0700

    refine web design

--- frontend/src/ChatsPage.jsx
+++ frontend/src/ChatsPage.jsx
@@ -1,18 +1,15 @@
-import { MultiChatSocket, MultiChatWindow, useMultiChatLogic } from 'react-chat-engine-advanced'
+import { PrettyChatWindow} from 'react-chat-engine-pretty'
 
 const ChatsPage = (props) => {
-
-    const chatProps = useMultiChatLogic( 
-        '3c4c86ce-8ae6-4911-8865-e374bd9f806a', 
-        props.user.username, 
-        props.user.secret
-    );
-
     return (
-    <div style={{ height: '100vh' }}>
-        <MultiChatSocket {...chatProps} />
-        <MultiChatWindow {...chatProps} style={{height: '100%'}} />
-    </div>
+        <div style={{height: '100vh'}}>
+            < PrettyChatWindow 
+                projectId= '3c4c86ce-8ae6-4911-8865-e374bd9f806a'
+                usename= {props.user.username} 
+                secret={props.user.secret}
+                style={{height: '100%'}}
+            />
+        </div>
     )
 }
 
